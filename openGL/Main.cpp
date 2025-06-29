c59b7a31ef801a6ae7ecbf137603384b1244f41d
aleurankar <aleurankar@DEV-MACHINE>
2025-06-29 13:41:09 +0200

    better window

--- openGL/Main.cpp
+++ openGL/Main.cpp
@@ -1,52 +1,35 @@
 #include <iostream>
-#include <stdexcept>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main()
 {
-	try
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Window", nullptr, nullptr);
+	if (window == NULL)
 	{
-		if (!glfwInit()) 
-		{
-			throw std::runtime_error("Failed to initialize GLFW");
-		}
-
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-		GLFWwindow* window = glfwCreateWindow(800, 800, "Window", nullptr, nullptr);
-		if (window == nullptr)
-		{
-			glfwTerminate();
-			throw std::runtime_error("Failed to create GLFW window");
-		}
-
-		glfwMakeContextCurrent(window);
-		if (!gladLoadGL()) 
-		{
-			glfwTerminate();
-			throw std::runtime_error("Failed to initialize GLAD");
-		}
-
-		glfwSwapInterval(1);
-		glViewport(0, 0, 800, 800);
-
-		while (!glfwWindowShouldClose(window))
-		{
-			glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-		}
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return 0;
-	}
-
-	catch (const std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << std::endl;
 		return -1;
 	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+	return 0;
 }
